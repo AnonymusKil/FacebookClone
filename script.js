2de37fd9024515348d8e19fb59131457e1e7f661
Anonymuskil <navadesignz11@gmail.com>
2025-03-19 03:11:11 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,28 @@
+const settingsMenu = document.querySelector(".settings-menu");
+const darkBTN = document.getElementById('dark-btn')
+function settingsMenuToggle() {
+    settingsMenu.classList.toggle("settings-menu-height");   
+}
+darkBTN.onclick = function(){
+    darkBTN.classList.toggle('dark-btn-on')
+    document.body.classList.toggle("dark-theme")
+
+
+    if(localStorage.getItem("theme") == "light"){
+        localStorage.setItem('theme', 'dark')
+    }else{
+        localStorage.setItem('theme', 'light')
+    }
+}
+if(localStorage.getItem("theme") == "light"){
+    darkBTN.classList.remove("dark-btn-on")
+    document.body.classList.remove("dark-theme")
+
+}else if(localStorage.getItem("theme") == "dark"){
+    darkBTN.classList.add("dark-btn-on")
+    document.body.classList.add("dark-theme")
+}else{
+    localStorage.setItem("theme", "light")
+}
+localStorage.setItem("theme", "dark");
+localStorage.getItem("theme")
